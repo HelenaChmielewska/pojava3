64daeb6c587a2c30f57c5d7624f509e0ba900107
Helena Chmielewska <helena.chmielewska05@gmail.com>
2025-03-31 13:14:08 +0200

    Initial commit

---
+++ pojava/src/pojava/HelloWorld.java
@@ -0,0 +1,10 @@
+package pojava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("Działa mi github!");
+	}
+
+}
