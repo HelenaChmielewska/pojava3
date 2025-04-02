036bcfc84287afdd83a10da805ccc03b46c274cd
Kinga Urban <kingaurban2004@gmail.com>
2025-04-02 15:05:10 +0200

    dodałam parę komentarzy

--- pojava/src/pojava/BackgroundPanel.java
+++ pojava/src/pojava/BackgroundPanel.java
@@ -7,13 +7,13 @@ public class BackgroundPanel extends JPanel {
 
     public BackgroundPanel(String imagePath) {
     	//wczytanie obrazu tła
-        backgroundImage = new ImageIcon(getClass().getResource(imagePath)).getImage();
+        backgroundImage = new ImageIcon(getClass().getResource(imagePath)).getImage(); //Image nie posiada konstruktora do wczytania pliku, łatwiej to zrobić przez ImageIcon
     }
     
  // nadpisanie paintComponent do rysowania obrazu tła
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g); //m.in. czyści panel
-        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this); //rysowanie obrazu na całej powierzchni panelu
+        g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this); //rysowanie obrazu na całej powierzchni panelu, argument this działa jak obserwator ładowania obrazu, gdy np. ładuje się asynchronicznie
     }
 }
