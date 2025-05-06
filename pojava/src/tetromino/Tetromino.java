77643ff90635daa77a2b952bd02b4562d95a1e38
Kinga Urban <kingaurban2004@gmail.com>
2025-05-06 19:08:59 +0200

    sprawdzanie kolizji klocków ze ścianami

--- pojava/src/tetromino/Tetromino.java
+++ pojava/src/tetromino/Tetromino.java
@@ -13,6 +13,7 @@ public class Tetromino {
 	public Block tempB[] = new Block[4]; //tymczasowe pozycje, do testowania chyba
 	int autoDropCounter = 0;
 	public int direction = 1; //4 kierunki: 1,2,3,4
+	boolean leftCollision, rightCollision, bottomCollision;
 	
 	public void create(Color color) { //zrobiłam metodę create zamiast konstruktora bo z konstruktorem nie działało potem w dziedziczących
         for (int i = 0; i < 4; i++) {
@@ -22,50 +23,113 @@ public class Tetromino {
 	}
 	public void setXY(int x, int y) {};
 	public void updateXY(int direction) {
-		this.direction = direction;
-		//przepisujemy do tablicy temp, żeby uniknąć kolizji - najpierw sprawdzamy czy można wykonać rotację
-		for (int i = 0; i < 4; i++) {
-            b[i].x = tempB[i].x;
-            b[i].y = tempB[i].y;
-        }
+		checkRotationCollision();
+		
+		if(leftCollision==false && rightCollision==false && bottomCollision==false) {
+			this.direction = direction;
+			//przepisujemy do tablicy temp, żeby uniknąć kolizji - najpierw sprawdzamy czy można wykonać rotację
+			for (int i = 0; i < 4; i++) {
+	            b[i].x = tempB[i].x;
+	            b[i].y = tempB[i].y;
+	        }
+		}
 		
 	}
 	public void getDirection1() {};
 	public void getDirection2() {};
 	public void getDirection3() {};
 	public void getDirection4() {};
+	public void checkMovementCollision() {
+		leftCollision = false; //resetujemy zmienne
+		rightCollision = false;
+		bottomCollision = false;
+		
+		//sprawdzamy kolizje z lewą ścianą
+		for (int i=0; i<b.length; i++) {
+			if (b[i].x ==PlayManager.left_x) {
+				leftCollision =true;
+			}		
+		}
+		//sprawdzamy kolizje z prawą ścianą
+		for (int i=0; i<b.length; i++) {
+			if (b[i].x + Block.size ==PlayManager.right_x) {
+				rightCollision =true;
+			}		
+		}
+		//sprawdzamy kolizje z dolną ścianą
+				for (int i=0; i<b.length; i++) {
+					if (b[i].y +Block.size ==PlayManager.bottom_y) {
+						bottomCollision =true;
+					}		
+				}
+		
+	}
+	public void checkRotationCollision() {
+		leftCollision = false; //resetujemy zmienne
+		rightCollision = false;
+		bottomCollision = false;
+		
+		//sprawdzamy kolizje z lewą ścianą
+		for (int i=0; i<b.length; i++) {
+			if (tempB[i].x < PlayManager.left_x) {
+				leftCollision =true;
+			}		
+		}
+		//sprawdzamy kolizje z prawą ścianą
+		for (int i=0; i<b.length; i++) {
+			if (tempB[i].x + Block.size > PlayManager.right_x) {
+				rightCollision =true;
+			}		
+		}
+		//sprawdzamy kolizje z dolną ścianą
+		for (int i=0; i<b.length; i++) {
+			if (tempB[i].y +Block.size > PlayManager.bottom_y) {
+				bottomCollision =true;
+			}		
+		}
+	}
+	
 	public void update() {
 		synchronized(KeyHandler.INSTANCE) {
-		//sterowanie tetrominem
-		if(KeyHandler.INSTANCE.upPressed) {
-			switch(direction) {
-			case 1: getDirection2(); break;
-			case 2: getDirection3(); break;
-			case 3: getDirection4(); break;
-			case 4: getDirection1(); break;
+			//sterowanie tetrominem
+			if(KeyHandler.INSTANCE.upPressed) {
+				switch(direction) {
+				case 1: getDirection2(); break;
+				case 2: getDirection3(); break;
+				case 3: getDirection4(); break;
+				case 4: getDirection1(); break;
+				}
+				KeyHandler.INSTANCE.upPressed = false;
 			}
-			KeyHandler.INSTANCE.upPressed = false;
-		}
-		if(KeyHandler.INSTANCE.downPressed) {
-			for(int i=0; i<4; i++) {
-				b[i].y +=Block.size; //przesuwamy klocek do dołu o jedno pole
-			}
-			autoDropCounter = 0; //gdy strzałka w dół resetujemy autodrop
-			KeyHandler.INSTANCE.downPressed = false; //po przesunięciu klocka w dół zmieniamy na false
-		}
-		if(KeyHandler.INSTANCE.leftPressed) {
-			for(int i=0; i<4; i++) {
-				b[i].x -=Block.size; //przesuwamy klocek w lewo o jedno pole
-			}
-			KeyHandler.INSTANCE.leftPressed = false;
-		}
-		if(KeyHandler.INSTANCE.rightPressed) {
-			for(int i=0; i<4; i++) {
-				b[i].x +=Block.size; //przesuwamy klocek w prawo o jedno pole
-			}
-			KeyHandler.INSTANCE.rightPressed = false;
+			checkMovementCollision(); //zanim poruszymy klockiem sprawdzamy czy nie ma kolizji ze ścianami
 			
-		}
+			if(KeyHandler.INSTANCE.downPressed) {
+				if (bottomCollision ==false) { //możemy poruszyć klockiem tylko gdy nie ma kolizji
+				for(int i=0; i<4; i++) {
+					b[i].y +=Block.size; //przesuwamy klocek do dołu o jedno pole
+				}
+				autoDropCounter = 0; //gdy strzałka w dół resetujemy autodrop
+				}
+				
+				KeyHandler.INSTANCE.downPressed = false; //po przesunięciu klocka w dół zmieniamy na false
+			}
+			if(KeyHandler.INSTANCE.leftPressed) {
+				if(leftCollision==false) {
+					for(int i=0; i<4; i++) {
+					b[i].x -=Block.size; //przesuwamy klocek w lewo o jedno pole
+					}
+				}
+				KeyHandler.INSTANCE.leftPressed = false;
+			}
+			if(KeyHandler.INSTANCE.rightPressed) {
+				if(rightCollision==false) {
+					for(int i=0; i<4; i++) {
+					b[i].x +=Block.size; //przesuwamy klocek w prawo o jedno pole
+					}
+				}
+				KeyHandler.INSTANCE.rightPressed = false;
+				
+			}
 		}
 		
 		//automatyczne spadanie
